f21916994f22c2cc1e454d70b6d2c1e72f507295
Ilya Nee <neeilya@icloud.com>
2018-12-05 13:07:32 +0000

    4b2b2ed v5.0.16

---
+++ TranslateMenuItem.js
@@ -0,0 +1,117 @@
+import _Object$getPrototypeOf from 'babel-runtime/core-js/object/get-prototype-of';
+import _classCallCheck from 'babel-runtime/helpers/classCallCheck';
+import _createClass from 'babel-runtime/helpers/createClass';
+import _possibleConstructorReturn from 'babel-runtime/helpers/possibleConstructorReturn';
+import _inherits from 'babel-runtime/helpers/inherits';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuItem from '@material-ui/core/MenuItem';
+import Translate from '@material-ui/icons/Translate';
+
+import i18n from '@dhis2/d2-i18n';
+import TranslationDialog from '@dhis2/d2-ui-translation-dialog';
+
+var TranslateMenuItem = function (_Component) {
+    _inherits(TranslateMenuItem, _Component);
+
+    function TranslateMenuItem(props) {
+        _classCallCheck(this, TranslateMenuItem);
+
+        var _this = _possibleConstructorReturn(this, (TranslateMenuItem.__proto__ || _Object$getPrototypeOf(TranslateMenuItem)).call(this, props));
+
+        _this.onClose = function () {
+            _this.toggleTranslationDialog();
+
+            if (_this.props.onClose) {
+                _this.props.onClose();
+            }
+        };
+
+        _this.onDialogReturn = function (success) {
+            return function (args) {
+                var _this$props = _this.props,
+                    onTranslate = _this$props.onTranslate,
+                    onTranslateError = _this$props.onTranslateError;
+
+
+                _this.toggleTranslationDialog();
+
+                if (success && onTranslate) {
+                    onTranslate(args);
+                } else if (onTranslateError) {
+                    onTranslateError(args);
+                }
+            };
+        };
+
+        _this.toggleTranslationDialog = function () {
+            _this.setState({ dialogIsOpen: !_this.state.dialogIsOpen });
+        };
+
+        _this.state = {
+            dialogIsOpen: false
+        };
+        return _this;
+    }
+
+    _createClass(TranslateMenuItem, [{
+        key: 'render',
+        value: function render() {
+            var _props = this.props,
+                enabled = _props.enabled,
+                fileModel = _props.fileModel;
+
+
+            return React.createElement(
+                Fragment,
+                null,
+                React.createElement(
+                    MenuItem,
+                    { disabled: !enabled, onClick: this.toggleTranslationDialog },
+                    React.createElement(
+                        ListItemIcon,
+                        null,
+                        React.createElement(Translate, null)
+                    ),
+                    React.createElement(ListItemText, { primary: i18n.t('Translate') })
+                ),
+                fileModel ? React.createElement(TranslationDialog, {
+                    d2: this.context.d2,
+                    open: this.state.dialogIsOpen,
+                    onRequestClose: this.onClose,
+                    objectToTranslate: fileModel,
+                    fieldsToTranslate: ['name', 'description'],
+                    onTranslationSaved: this.onDialogReturn(true),
+                    onTranslationError: this.onDialogReturn(false)
+                }) : null
+            );
+        }
+    }]);
+
+    return TranslateMenuItem;
+}(Component);
+
+TranslateMenuItem.contextTypes = {
+    d2: PropTypes.object
+};
+
+TranslateMenuItem.defaultProps = {
+    enabled: false,
+    fileModel: null,
+    onTranslate: null,
+    onTranslateError: null,
+    onClose: null
+};
+
+TranslateMenuItem.propTypes = {
+    enabled: PropTypes.bool,
+    fileModel: PropTypes.object,
+    onTranslate: PropTypes.func,
+    onTranslateError: PropTypes.func,
+    onClose: PropTypes.func
+};
+
+export default TranslateMenuItem;
