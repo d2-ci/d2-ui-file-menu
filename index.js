7b7670efe74d1545dcad729b0e0f316254961bf7
Edoardo Sabadelli <edoardo@dhis2.org>
2019-11-05 13:43:44 +0000

    e6af2f4 fix: fix i18n dependency for generating locales

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _FileMenu = require('./FileMenu');
+
+var _FileMenu2 = _interopRequireDefault(_FileMenu);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _FileMenu2.default;
