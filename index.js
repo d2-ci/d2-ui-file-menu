710ef577c558f86a7c76c28d7d5032f0cbca3711
Jen Jones Arnesen <jennifer@dhis2.org>
2020-04-29 16:44:17 +0000

    961b56b fix: favorites-dialog needs transifex config

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+require("./locales/");
+
+var _FileMenu = require("./FileMenu");
+
+var _FileMenu2 = _interopRequireDefault(_FileMenu);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _FileMenu2.default;
